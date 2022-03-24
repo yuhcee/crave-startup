4d0690926a5f7c979b79684777cc3ff8eb38df10
yuhcee <egbouchenna001@gmail.com>
2022-03-24 23:12:36 +0100

    add mock data

---
+++ src/utils.js
@@ -0,0 +1,28 @@
+export const TodoConfig = [
+    {
+        title: 'Foundation',
+        isCompleted: false,
+        subItems: [
+            { text: 'Setup virtual office', isCompleted: false },
+            { text: 'Set mission and vision', isCompleted: false },
+            { text: 'Select business name', isCompleted: false },
+            { text: 'Buy domains', isCompleted: false },
+        ],
+    },
+    {
+        title: 'Discovery',
+        isCompleted: false,
+        subItems: [
+            { text: 'Create roadmap', isCompleted: false },
+            { text: 'Competitor analysis', isCompleted: false },
+        ],
+    },
+    {
+        title: 'Delivery',
+        isCompleted: false,
+        subItems: [
+            { text: 'Release marketing website', isCompleted: false },
+            { text: 'Release MVP', isCompleted: false },
+        ],
+    },
+];

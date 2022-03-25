79d651836353e2111f59ba771a5d11f3e0739db7
yuhcee <egbouchenna001@gmail.com>
2022-03-25 13:09:52 +0100

    add React ContextAPI

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import TodoProvider from './context/TodoContext';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <TodoProvider>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </TodoProvider>,
+    document.getElementById('root')
 );
 reportWebVitals();
